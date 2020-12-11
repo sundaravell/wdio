95221fde1517a751092a92b78639a2d25363c18f
sundaravell <sundaravel.loganathan.imaginea.com>
2020-12-11 15:03:17 +0530

    update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,12 +52,12 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
-        'goog:chromeOptions': {
+        browserName: 'firefox',
+        //'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-            args: ['--headless', '--disable-gpu'],
-            },
+          //  args: ['--headless', '--disable-gpu'],
+            //},
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
