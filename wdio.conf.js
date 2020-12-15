a48c32547ad2e116ad47305f0e75527190b53189
sundaravell <sundaravel.loganathan.imaginea.com>
2020-12-15 14:30:54 +0530

    chrome wdio change

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'firefox',
+        browserName: 'chrome',
         //'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
