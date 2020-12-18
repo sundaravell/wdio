6fe9bac0b664b3ed1ed0291245195f66f6e19a65
sundaravell <sundaravel.loganathan.imaginea.com>
2020-12-18 16:19:40 +0530

    updated gui/headless logic

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,4 @@
-let gui = process.env.GUI
-
-let noHeadless = 'chrome'
+let gui = process.env.gui;
 
 exports.config = {
     //
@@ -56,10 +54,12 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: gui === true ? noHeadless : 'chrome',
-        'goog:chromeOptions': {
-            args: ['--headless', '--disable-gpu'],
-        },
+        browserName: 'chrome',
+        'goog:chromeOptions': gui === "true" ? {} :
+
+            {
+                args: ['--headless', '--disable-gpu','--window-size=1920,1080'],
+            },
         /*browserName: 'chrome',
         'goog:chromeOptions': {
         // to run chrome headless the following flags are required
